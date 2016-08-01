64b6dd4f9ea25d24d49c5a0c58bfb26b29d4a531
= <=>
2016-08-01 14:41:12 -0300

    Exemplificando rotas com response

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
 
+use Symfony\Component\HttpFoundation\Response;
+
+$response = new Response();
+
+//  Definindo Rota do tipo GET
+$app->get('/', function () use ($response) {
+
+    // Trabalhando com Response
+    $response->setContent('Olá Mundo');
+    return $response;
+
+});
+
 $app->run();
